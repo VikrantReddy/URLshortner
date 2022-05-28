dee148bebf16921f2ee85bd4c21f8ce2d9d32f31
Vikrant Reddy <vikrantreddy@hotmail.com>
2022-05-28 14:05:53 +0530

    Client Server integration

--- server/server.js
+++ server/server.js
@@ -2,13 +2,26 @@ const express = require("express");
 const shorts = require("./routes/shorts");
 const user = require("./routes/user");
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.set("view engine", "hbs");
+app.use(express.json());
+
 app.use("/user", user);
 app.use("/shorts", shorts);
 
-app.get("/", (req, res) => {
+app.use((req, res, next) => {
+  console.log("Recieved Request: " + req.url);
+  next();
+});
+
+app.post("/hello", (req, res) => {
+  console.log(req.body);
+  res.send("Hello World!");
+});
+
+app.post("/shorten", (req, res) => {
+  console.log(req.body);
   res.send("Hello World!");
 });
 
